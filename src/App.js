7a9b4d4d9ecda37c7692c1435d9bb1f5884fc873
ashish-lamsal <ashishlamsal4283@gmail.com>
2022-01-12 16:44:31 +0545

    editor initialized in react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import { Typography, Container, Box } from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
+import React from "react";
+import Editor from "./components/Editor";
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <React.Fragment>
+      <CssBaseline enableColorScheme />
+      <Container sx={{ minHeight: "100vh" }} maxWidth="md" disableGutters>
+        <Box
+          style={{
+            backgroundColor: "#ebebeb",
+            minHeight: "100vh",
+          }}
+        >
+          <Typography
+            variant="h4"
+            component="h1"
+            sx={{ textAlign: "center", py: 3 }}
+          >
+            Realtime Text Similarity Identification
+          </Typography>
+          <Editor />
+        </Box>
+      </Container>
+    </React.Fragment>
+  );
 }
 
 export default App;
