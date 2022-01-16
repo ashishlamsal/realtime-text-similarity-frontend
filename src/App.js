df7c24b8c88b5ce651e1f2e4fe7c92d11065e7f7
Aayush Neupane <neupane0403@gmail.com>
2022-01-16 22:27:38 +0545

    questions in the list

--- src/App.js
+++ src/App.js
@@ -1,53 +1,40 @@
-import { Typography, Container, Box } from "@mui/material";
-import CssBaseline from "@mui/material/CssBaseline";
-import React,{useState} from "react";
-import Editor from "./components/Editor";
-import PredictionList from "./components/predictionList";
-import Editor2 from "./components/Editor";
+import { Typography, Container, Box } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
+import React, { useState } from 'react';
+import Editor from './components/Editor';
+import PredictionList from './components/predictionList';
+import Editor2 from './components/Editor';
 
 function App() {
-  const [num,setNum]=useState(1);
-  return (
-    <React.Fragment>
-      <CssBaseline enableColorScheme />
-      <Container sx={{ minHeight: "100vh" }} maxWidth="md" disableGutters>
-        <Box
-          style={{
-            backgroundColor: "#ebebeb",
-            minHeight: "100vh",
-          }}
-        >
-          <Typography
-            variant="h4"
-            component="h1"
-            sx={{ textAlign: "center", py: 3 }}
-          >
-            Realtime Text Similarity Identification
-          </Typography>
-          {Array(num).fill(0).map((_, i) =><Editor/>)}
-          <p onClick={()=>setNum(num+1)}>Add New Question</p>
-          {/* <PredictionList/> */}
-        </Box>
-      </Container>
-    </React.Fragment>
-  );
+	const [questions, setQuestions] = useState(['']);
+	return (
+		<React.Fragment>
+			<CssBaseline enableColorScheme />
+			<Container sx={{ minHeight: '100vh' }} maxWidth="md" disableGutters>
+				<Box
+					style={{
+						backgroundColor: '#ebebeb',
+						minHeight: '100vh',
+					}}
+				>
+					<Typography
+						variant="h4"
+						component="h1"
+						sx={{ textAlign: 'center', py: 3 }}
+					>
+						Realtime Text Similarity Identification
+					</Typography>
+					{questions.map((question, i) => (
+						<Editor text={question} />
+					))}
+					<p onClick={() => setQuestions([...questions, ''])}>
+						Add New Question
+					</p>
+					{/* <PredictionList/> */}
+				</Box>
+			</Container>
+		</React.Fragment>
+	);
 }
 
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,13 +1,17 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useEditor, EditorContent } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
 import './styles.scss';
 import PredictionList from './predictionList';
 import { onBlur } from 'draft-js/lib/DraftEditorEditHandler';
 
-const Abc = () => {
+const Editor = ({text}) => {
 	const [inputFocused, setInputFocused] = useState(false);
   const [question,setQuestion]=useState("");
+
+  useEffect(()=>{
+	setQuestion(text);
+  },[text])
 
 	return (
 		<div>
@@ -16,9 +20,10 @@ const Abc = () => {
 				onFocus={() => setInputFocused(true)}
 				onBlur={() => setInputFocused(false)}
         onChange={(event)=>setQuestion(event.target.value)}
+		value={question}
 			/>
 			{inputFocused && <PredictionList question={question}/>}
 		</div>
 	);
 };
-export default Abc;
+export default Editor;
